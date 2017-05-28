5a3eafe2e150c53b3f7e1af0dd584aba0a7f4ef4
chendong <1101873740@qq.com>
2017-05-28 13:45:11 +0800

    update 编译命令行工具尝试1

---
+++ app/src/main/obj/local/armeabi-v7a/objs/ffmpeg_support/ffmpeg_opt.o.d
@@ -0,0 +1,140 @@
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/obj/local/armeabi-v7a/objs/ffmpeg_support/ffmpeg_opt.o: \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/ffmpeg_opt.c \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/ffmpeg.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/config.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/cmdutils.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavcodec/avcodec.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/samplefmt.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avutil.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/common.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/attributes.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/version.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/macros.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avconfig.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/mem.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/error.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/rational.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/mathematics.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/intfloat.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/log.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/pixfmt.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/old_pix_fmts.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/buffer.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/cpu.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/channel_layout.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/dict.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/frame.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavcodec/version.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavcodec/old_codec_ids.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavfilter/avfilter.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavfilter/version.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavformat/avformat.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavformat/avio.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavformat/version.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswscale/swscale.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswscale/version.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/eval.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/fifo.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/threadmessage.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswresample/swresample.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswresample/version.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avassert.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avstring.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/intreadwrite.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/bswap.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/opt.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/parseutils.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/pixdesc.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/cmdutils_common_opts.h
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/ffmpeg.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/config.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/cmdutils.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavcodec/avcodec.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/samplefmt.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avutil.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/common.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/attributes.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/version.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/macros.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avconfig.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/mem.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/error.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/rational.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/mathematics.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/intfloat.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/log.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/pixfmt.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/old_pix_fmts.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/buffer.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/cpu.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/channel_layout.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/dict.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/frame.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavcodec/version.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavcodec/old_codec_ids.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavfilter/avfilter.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavfilter/version.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavformat/avformat.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavformat/avio.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavformat/version.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswscale/swscale.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswscale/version.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/eval.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/fifo.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/threadmessage.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswresample/swresample.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswresample/version.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avassert.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avstring.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/intreadwrite.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/bswap.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/opt.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/parseutils.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/pixdesc.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/cmdutils_common_opts.h:
---
+++ app/src/main/obj/local/armeabi/objs/ffmpeg_support/ffmpeg_filter.o.d
@@ -0,0 +1,143 @@
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/obj/local/armeabi/objs/ffmpeg_support/ffmpeg_filter.o: \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/ffmpeg_filter.c \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/ffmpeg.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/config.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/cmdutils.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavcodec/avcodec.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/samplefmt.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avutil.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/common.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/attributes.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/version.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/macros.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avconfig.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/mem.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/error.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/rational.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/mathematics.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/intfloat.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/log.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/pixfmt.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/old_pix_fmts.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/buffer.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/cpu.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/channel_layout.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/dict.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/frame.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavcodec/version.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavcodec/old_codec_ids.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavfilter/avfilter.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavfilter/version.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavformat/avformat.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavformat/avio.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavformat/version.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswscale/swscale.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswscale/version.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/eval.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/fifo.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/threadmessage.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswresample/swresample.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswresample/version.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavfilter/buffersink.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavresample/avresample.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavresample/version.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avassert.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avstring.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/bprint.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/opt.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/pixdesc.h \
+  /Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/imgutils.h
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/ffmpeg.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/config.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/cmdutils.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavcodec/avcodec.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/samplefmt.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avutil.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/common.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/attributes.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/version.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/macros.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avconfig.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/mem.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/error.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/rational.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/mathematics.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/intfloat.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/log.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/pixfmt.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/old_pix_fmts.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/buffer.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/cpu.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/channel_layout.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/dict.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/frame.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavcodec/version.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavcodec/old_codec_ids.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavfilter/avfilter.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavfilter/version.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavformat/avformat.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavformat/avio.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavformat/version.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswscale/swscale.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswscale/version.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/eval.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/fifo.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/threadmessage.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswresample/swresample.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libswresample/version.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavfilter/buffersink.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavresample/avresample.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavresample/version.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avassert.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/avstring.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/bprint.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/opt.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/pixdesc.h:
+
+/Users/march/AndroidPro/FFmpegAndroidSupport/app/src/main/jni/include/libavutil/imgutils.h:
